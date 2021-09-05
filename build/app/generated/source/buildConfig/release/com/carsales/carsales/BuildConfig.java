7903a8a9a9c5bfdd2516a104e5497de0b8cc04c3
maximilian putz <putzmaximilian@gmail.com>
2021-09-05 11:13:27 +0200

    web app builded2

---
+++ build/app/generated/source/buildConfig/release/com/carsales/carsales/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.carsales.carsales;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.carsales.carsales";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 4001;
+  public static final String VERSION_NAME = "1.0.0";
+}
